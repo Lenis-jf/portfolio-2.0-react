9559d47fd0e4f32f6998a9c62f26ad2c47ba32f7
juanfelenis <lenisserna2001@gmail.com>
2025-03-27 22:12:15 +0100

    fixing videos URLs and preparing to push with git LFS tracked archieves

--- src/pages/Batatabit.jsx
+++ src/pages/Batatabit.jsx
@@ -27,7 +27,7 @@ function Batatabit() {
                 <p>This project was made by me and a team of other students as a project for the class “OOP in java”. The idea was to create a GUI so that a final user could retrieve specific live  information about his drone(s).</p>
                 <a href="https://github.com/Lenis-jf/Drone-Project" className="button">Show Repository</a>
                 <p>Our final java application establishes a connection with an API provided by the teacher of the class to retrieve all the available information about the drones posted on the API. </p>
-                <CustomVideoPlayer video="https://drive.google.com/uc?export=download&id=1P6WX8VNzySYRauLqywp-SuFRvHhaCL90" poster="batatabit-assets/batatabit-img-1.png" />
+                <CustomVideoPlayer video="public/assets/videos/batatabit-project.mp4" poster="batatabit-assets/batatabit-img-1.png" />
                 <p>Besides the API connection gets refreshed either automatically every 5 minutes or the user refreshes it through a refresh button. In order to establish the connection successfully, the user must be connected to the university's WiFi   </p>
                 <span className="copy-right">©juanfelenis 2025</span>
             </section>
