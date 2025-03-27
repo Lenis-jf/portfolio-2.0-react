a8a8055cd6b158893d342a31d6c6cd82ce65e419
juanfelenis <lenisserna2001@gmail.com>
2025-03-27 22:32:37 +0100

    updating big videos URLs with direct URL from github

--- src/pages/OceanoRosa.jsx
+++ src/pages/OceanoRosa.jsx
@@ -28,7 +28,7 @@ function OceanoRosa() {
                 <p>This project was made by me and a team of other students as a project for the class “OOP in java”. The idea was to create a GUI so that a final user could retrieve specific live  information about his drone(s).</p>
                 <a href="https://github.com/Lenis-jf/Drone-Project" className="button">Show Repository</a>
                 <p>Our final java application establishes a connection with an API provided by the teacher of the class to retrieve all the available information about the drones posted on the API. </p>
-                <CustomVideoPlayer video="oceano-rosa-project.mp4" poster="oceano-rosa-assets/oceano-rosa-img-1.png" />
+                <CustomVideoPlayer video="https://github.com/Lenis-jf/portfolio-2.0-react/raw/refs/heads/main/public/assets/videos/oceano-rosa-project.mp4?download=" poster="oceano-rosa-assets/oceano-rosa-img-1.png" />
                 <p>Besides the API connection gets refreshed either automatically every 5 minutes or the user refreshes it through a refresh button. In order to establish the connection successfully, the user must be connected to the university's WiFi   </p>
                 <span className="copy-right">©juanfelenis 2025</span>
             </section>
--- src/pages/Tyc.jsx
+++ src/pages/Tyc.jsx
@@ -26,7 +26,7 @@ function Tyc() {
                 <p>This project was made by me and a team of other students as a project for the class “OOP in java”. The idea was to create a GUI so that a final user could retrieve specific live  information about his drone(s).</p>
                 <a href="https://github.com/Lenis-jf/Drone-Project" className="button">Show Repository</a>
                 <p>Our final java application establishes a connection with an API provided by the teacher of the class to retrieve all the available information about the drones posted on the API. </p>
-                <CustomVideoPlayer video="tyc-project.mp4" poster="tyc-assets/tyc-img-1.png" />
+                <CustomVideoPlayer video="https://github.com/Lenis-jf/portfolio-2.0-react/raw/refs/heads/main/public/assets/videos/tyc-project.mp4?download=" poster="tyc-assets/tyc-img-1.png" />
                 <p>Besides the API connection gets refreshed either automatically every 5 minutes or the user refreshes it through a refresh button. In order to establish the connection successfully, the user must be connected to the university's WiFi   </p>
                 <span className="copy-right">©juanfelenis 2025</span>
             </section>
